90c27641fbf6f5597ef75058897082271886be72
Steven Silvey <steven.silvey@NWASSILVEY3.local>
2013-01-12 22:40:39 -0600

    Added a snake animal

---
+++ Food Trap/Classes/Entities/Snake.h
@@ -0,0 +1,13 @@
+//
+//  Snake.h
+//  Food Trap
+//
+//  Created by Steven Silvey on 1/12/13.
+//  Copyright (c) 2013 Rockfish. All rights reserved.
+//
+
+#import "Animal.h"
+
+@interface Snake : Animal
+
+@end
---
+++ Food Trap/Classes/Entities/Snake.m
@@ -0,0 +1,12 @@
+//
+//  Snake.m
+//  Food Trap
+//
+//  Created by Steven Silvey on 1/12/13.
+//  Copyright (c) 2013 Rockfish. All rights reserved.
+//
+
+#import "Snake.h"
+
+@implementation Snake
+@end
